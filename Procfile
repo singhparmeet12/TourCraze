d4b3a9b4d2c6beae0e855506161cc43de0a2676a
Parmeet <parmeetssms@gmail.com>
2026-08-21 17:57:02 +0530

    build: add production requirements.txt, Procfile and WhiteNoise static handling

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn --chdir tourCraze tourCraze.wsgi:application --log-file -
--- tourCraze/tourCraze/settings.py
+++ tourCraze/tourCraze/settings.py
@@ -50,6 +50,7 @@ AUTH_USER_MODEL = "base.CusUser"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
